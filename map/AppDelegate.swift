f56c1a2469303901999d10cc0e49150bcd392b45
swimme <2sy100@likelion.org>
2020-08-12 20:36:44 +0900

    add .gitignore

--- map/AppDelegate.swift
+++ map/AppDelegate.swift
@@ -15,7 +15,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-           GMSServices.provideAPIKey("")
+           GMSServices.provideAPIKey(GOOGLE_API_KEY)
            // Override point for customization after application launch.
            return true
        }
---
+++ map/secrets.swift
@@ -0,0 +1,9 @@
+//
+//  secrets.swift
+//  map
+//
+//  Created by YOUNG on 2020/08/12.
+//  Copyright © 2020 user. All rights reserved.
+//
+
+import Foundation
